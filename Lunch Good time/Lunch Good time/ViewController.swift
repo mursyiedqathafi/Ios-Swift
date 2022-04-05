bb837798dd5d12c68b62b640b1a397dd7193e29d
Mursyied Qathafi <38604611+mursyiedqathafi@users.noreply.github.com>
2022-04-05 12:15:17 +0700

    Add files via upload

---
+++ Lunch Good time/Lunch Good time/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lunch Good time
+//
+//  Created by Mursyied Qathafi on 05/04/22.
+//  Copyright © 2022 Mursyied Qathafi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
